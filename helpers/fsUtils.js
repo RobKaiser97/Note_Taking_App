9cb9b864c7d85247426c7ef9c57e4dcd6dff9ea2
Robert Kaiser <paintballmanrob@gmail.com>
2023-09-10 12:36:03 -0400

    Added delete note functionality

--- helpers/fsUtils.js
+++ helpers/fsUtils.js
@@ -31,4 +31,17 @@ const readAndAppend = (content, file) => {
   });
 };
 
-module.exports = { readFromFile, writeToFile, readAndAppend };
+const readAndDelete = (id, file) => {
+  fs.readFile(file, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      let note = JSON.parse(data);
+      note = note.filter((note) => note.id !== id);
+      writeToFile(file, note);
+    }
+  });
+};
+
+module.exports = { readFromFile, writeToFile, readAndAppend, readAndDelete };
+
